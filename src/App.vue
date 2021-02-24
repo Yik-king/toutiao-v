98d092ffbdf6fcaa39ab64ec9830383062256e0d
weiyang0929 <8155066+weiyang0929@user.noreply.gitee.com>
2021-02-24 17:25:58 +0800

    导入vant

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <router-view />
-    <h1>黑马头条</h1>
+    <h1>头条</h1>
+    <i class="toutiao toutiao-shoucang"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import store from './store'
 // 导入全局样式
 import './style/index.less'
 
+// 导入vant组件库
+import Vant from 'vant'
+// 导入vant样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
